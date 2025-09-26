22be410389ff4ba2956bd2e4392b31b540e37c2a
Lakshit Goyal <lakshitgoyal111@gmail.com>
2025-09-27 01:39:24 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,6 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isProtectedRoute = createRouteMatcher([
   '/media-analysis(.*)',
-  '/fake-index(.*)',
   '/training(.*)',
   '/verification-badge(.*)',
 ]); 
